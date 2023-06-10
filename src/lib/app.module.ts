2704c50557ff0194664665e5cd9483d73a04b260
AlexStorm <info@alexstorm.ru>
2023-06-10 21:24:15 +0530

    fix build

--- src/lib/app.module.ts
+++ src/lib/app.module.ts
@@ -16,7 +16,7 @@ import {
     UITemplatesModule
 } from "@solenopsys/ui-templates";
 import {ClusterState} from "@solenopsys/fl-clusters";
-import {createNgxs, DataStorageModule} from "@solenopsys/fl-storage";
+import { DataStorageModule} from "@solenopsys/fl-storage";
 import {RowsState, UIListsModule} from "@solenopsys/ui-lists";
 import {DataHstreamModule, HStreamService, HStreamsState, StreamsPool, WsPool} from "@solenopsys/fl-hyperstreams";
 import {MountModule} from "./mount.module";
@@ -24,18 +24,39 @@ import {PluginsComponent} from "./plugins/plugins.component";
 import {UIFormsModule} from "@solenopsys/ui-forms";
 import {HelmRepositoriesState} from "@solenopsys/fl-helm";
 import {InstallationsState} from "@solenopsys/fl-installer";
-import {NGXS_PLUGINS, Store} from "@ngxs/store";
-import {NgxsLoggerPlugin} from "@ngxs/logger-plugin";
+import {NGXS_PLUGINS, NgxsModule, NoopNgxsExecutionStrategy, Store} from "@ngxs/store";
+import {NgxsLoggerPlugin, NgxsLoggerPluginModule} from "@ngxs/logger-plugin";
 import {RouterModule} from "@angular/router";
 import {Subject} from "rxjs";
 import {ColorSchemesService} from "@solenopsys/ui-themes";
 import {ClustersMenuProvider} from "./cluster-menu-provider";
 import {NewClusterComponent} from "./new-cluster/new-cluster";
 import {UIControlsModule} from "@solenopsys/ui-controls";
+import {NgxsRouterPluginModule} from "@ngxs/router-plugin";
 
 
 export const STATES=[InterfaceState, MenuState, ClusterState, GridState, RowsState, HStreamsState, HelmRepositoriesState, InstallationsState]
 const menu$ = new Subject()
+
+export function createNgxs(develop = false, stores = [], forRoot = false): any[] {
+    return [
+        forRoot ? NgxsModule.forRoot(
+                [ ...stores],
+                {
+                    developmentMode: develop,
+                    selectorOptions: {injectContainerState: true},
+                    executionStrategy: NoopNgxsExecutionStrategy
+                }) :
+            NgxsModule.forFeature(
+                [ ...stores],
+            ),
+        NgxsLoggerPluginModule.forRoot(),
+        NgxsRouterPluginModule.forRoot(),
+        //  NgxsReduxDevtoolsPluginModule.forRoot()
+        //   NgxsFormPluginModule.forRoot(),
+    ]
+}
+
 
 export function ensureRoutesExist(
     http: ModulesService,
@@ -82,7 +103,8 @@ export class LoadingComponent {
         UITemplatesModule,
         UIListsModule,
         UIFormsModule,
-        UIControlsModule
+        UIControlsModule,
+        ...createNgxs(false, STATES, true),
 
     ],
     providers: [WsPool, HStreamService, StreamsPool,
