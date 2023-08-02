9ddc6149ebf831f808c1960f454ec35d84c0480a
AlexStorm <info@alexstorm.ru>
2023-08-02 12:53:19 +0530

    entry refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export * from './lib/app.module';
+
+import {AppModule} from "./lib/app.module";
+import {XsModule, XsModuleType} from "@solenopsys/fl-globals";
+
+export const ENTRY:XsModule<AppModule> ={
+    module: AppModule,
+    type: XsModuleType.CLUSTER,
+};
